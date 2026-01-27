a03a6b46f5ee9ceaf0821bf65cd20e8d9c839f11
Adrian del Cid <adelc@esliceu.net>
2026-01-27 10:03:12 +0100

    Dia 3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,53 @@
-let currentPlayer = 'X';
+let isPlayerOne = true;
+let cells = document.getElementsByClassName("cell");
 
-
-function setBoard(){
-    let board = [document.querySelector("casella")];
-    for(let i = 0; i < board.lenght; i++){
-        let currentBox = board[i];
-        currentBox.onClick(() => {
-            //pintar jugada
-            //comprovar guanyador
-            //canviar jugador
-        });
-    }
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", userMove);
 }
+
+function userMove(e) {
+  let cellValue = e.target.innerHTML;
+
+  if (!cellValue.length) {
+    e.target.innerHTML = isPlayerOne ? "X" : "O";
+    isPlayerOne = !isPlayerOne;
+
+    checkLine(0, 1, 2);
+    checkLine(3, 4, 5);
+    checkLine(6, 7, 8);
+    checkLine(0, 3, 6);
+    checkLine(1, 4, 7);
+    checkLine(2, 5, 8);
+    checkLine(0, 4, 8);
+    checkLine(6, 4, 2);
+  }
+}
+
+function checkLine(c1, c2, c3) {
+  if (
+    cells[c1].innerHTML.length &&
+    cells[c1].innerHTML === cells[c2].innerHTML &&
+    cells[c2].innerHTML === cells[c3].innerHTML
+  ) {
+    showWinner(cells[c1].innerHTML);
+  }
+}
+
+function showWinner(player) {
+  document.getElementById("results").innerHTML = player + " gana ";
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", userMove);
+  }
+}
+
+document.getElementById("restart").addEventListener("click", () => {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerHTML = "";
+    cells[i].addEventListener("click", userMove);
+  }
+  document.getElementById("results").innerHTML = "";
+  isPlayerOne = true;
+});
+
+
